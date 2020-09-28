4889c0803d1441f4ed8e6c83386c8f3e0c70a9b3
sachinkm308 <sachinkm308@gmail.com>
2020-09-28 21:12:02 +0200

    edited parenthesis on print statements, works on tf v.15

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 	iters = []
 	iters_acc = []
 	
-	for i in range(1500):
+	for i in range(100): #Actual 1500
 				
 		batch_x, batch_y = mnist.train.next_batch(50)
 		batch_x = np.reshape(batch_x,(-1, 28, 28,1))
@@ -77,7 +77,7 @@ if __name__ == "__main__":
 			
 			# prune from time to time, finetune in the meantime
 			if i%500==0:
-				print 'iter:', i, 'prune weights'
+				print('iter:', i, 'prune weights')
 				for L in LAYERS:
 					L.prune_weights(sess, threshold=0.1)
 			
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 			
 			# quantize only once and then finetune
 			if i==1000:
-				print 'iter:', i, "quantize weights"
+				print('iter:', i, "quantize weights")
 				for L in LAYERS:
 					L.quantize_weights(sess)
 
@@ -137,7 +137,7 @@ if __name__ == "__main__":
 
 			iters.append(i)
 			iters_acc.append(acc)				
-			print 'iter:', i, 'test accuracy:', acc
+			print('iter:', i, 'test accuracy:', acc)
 
 			for L in LAYERS:
 				L.save_weights_histogram(sess, histograms_dir, i)
@@ -152,4 +152,4 @@ if __name__ == "__main__":
 	plt.plot(iters, iters_acc, color='0.4')
 	plt.savefig('./train_acc', dpi=1200)
 
-	print 'Training finished'
+	print('Training finished')
